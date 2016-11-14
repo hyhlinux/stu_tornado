8d1bee252d16a76b286949ae348d17e3df60125e
hyhlinux <2285020853@qq.com>
2016-11-14 22:13:36 +0800

    [tornado][模板][作用]
    plan:
    Type:
    Test:ok

--- demo/demo_static.py
+++ demo/demo_static.py
@@ -15,13 +15,64 @@ define("port", default=8000, type=int)
 class IndexHandler(RequestHandler):
 
     def get(self):
-        self.render("index.html")
+        house_info = {
+            "price": 398,
+            "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+            "score": 5,
+            "comments": 6,
+            "position": "北京市丰台区六里桥地铁"
+        }
+        # self.render("index.html", **house_info)
+        self.render("index.html", **house_info)
+
+
+class IndexAllHandler(RequestHandler):
+
+    def get(self):
+        houses = [
+            {
+                "price": 398,
+                "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+                "score": 5,
+                "comments": 6,
+                "position": "北京市丰台区六里桥地铁"
+            },
+            {
+                "price": 398,
+                "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+                "score": 5,
+                "comments": 6,
+                "position": "北京市丰台区六里桥地铁"
+            },
+            {
+                "price": 398,
+                "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+                "score": 5,
+                "comments": 6,
+                "position": "北京市丰台区六里桥地铁"
+            },
+            {
+                "price": 398,
+                "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+                "score": 5,
+                "comments": 6,
+                "position": "北京市丰台区六里桥地铁"
+            },
+            {
+                "price": 398,
+                "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+                "score": 5,
+                "comments": 6,
+                "position": "北京市丰台区六里桥地铁"
+            }]
+        self.render("index2.html", houses=houses)
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     tornado.options.parse_command_line()
     app = tornado.web.Application([
         (r"/", IndexHandler),
+        (r"/all", IndexAllHandler),
     ],
         static_path=os.path.join(os.path.dirname(__file__), "../static"),
         template_path=os.path.join(os.path.dirname(__file__), "../template"),
